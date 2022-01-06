cfd915c682c5eb0f105fe2773217cb88662e7927
Alex <foster-13@yandex.ru>
2022-01-06 16:18:56 +0300

    modal window

--- js/movies.js
+++ js/movies.js
@@ -3,7 +3,7 @@ const API_URL_POULAR =
   "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1";
 const API_URL_SEARCH =
   "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
-
+const API_FILM_INFO = "https://kinopoiskapiunofficial.tech/api/v2.2/films/";
 const titleSearch = document.querySelector('.title__films');
 
 getMovies(API_URL_POULAR);
@@ -17,9 +17,20 @@ async function getMovies(url) {
     },
   });
   const respData = await resp.json();
-  console.log(respData);
   showMovies(respData);
-}
+};
+
+async function getMovie(url) {
+  const resp = await fetch(url, {
+    method: "GET",
+    headers: {
+      "X-API-KEY": API_KEY,
+      "Content-Type": "application/json",
+    },
+  });
+  const respData = await resp.json();
+  showModal(respData);
+};
 
 function getClassByRate(vote) {
   if (vote > 7) {
@@ -29,7 +40,7 @@ function getClassByRate(vote) {
   } else {
     return "red";
   }
-}
+};
 
 function showMovies(data) {
   const moviesEl = document.querySelector(".movies");
@@ -39,50 +50,119 @@ function showMovies(data) {
   data.films.forEach((movie) => {
     const movieEl = document.createElement("div");
     movieEl.classList.add("movie");
+    movieEl.classList.add("popup_img");
     movieEl.innerHTML = `
-        <div class="movie">
-        <div class="movie__cover--inner">
-            <img src='${movie.posterUrlPreview}' alt="${
-                movie.nameRu
-            }" class="movie__cover">
-            <div class="movie__cover--darkened"></div>
-        </div>
-        <div class="movie__info">
-            <div class="movie__title">${movie.nameRu}</div>
-            <div class="movie__category">${movie.genres.map((genre, index) => {
-              if (index < 3) {
-                return `${genre.genre}`;
-              }
-            })}</div>
-            ${
-              movie.rating && 
-              `
-                <div class="movie__average movie__average--${getClassByRate(
-                    movie.rating
-                    )}">${movie.rating}</div>   
-                    `
-                    }
-                    
+        <div  class="movie card">
+          <div class="movie__cover--inner">
+              <img src='${movie.posterUrlPreview}' alt="${
+                  movie.nameRu
+              }" class="movie__cover">
+              <div class="movie__cover--darkened" data-id="${movie.filmId}"></div>
+          </div>
+          <div class="movie__info">
+              <div class="movie__title">${movie.nameRu}</div>
+              <div class="movie__category">${movie.genres.map((genre, index) => {
+                if (index < 3) {
+                  return `${genre.genre}`;
+                }
+              })}</div>
+              ${
+                movie.rating && 
+                `
+                  <div class="movie__average movie__average--${getClassByRate(
+                      movie.rating
+                      )}">${movie.rating}</div>   
+                `
+                }   
                 </div>
         </div>
         `;
-    moviesEl.appendChild(movieEl);
+    moviesEl.appendChild(movieEl);  
   });
-}
-
+  
+};
+// поиск фильмов 
 const form = document.querySelector("form");
 const search = document.querySelector(".form__search");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
   const apiSearchUrl = `${API_URL_SEARCH}${search.value}`;
-
   if (search.value) {
     getMovies(apiSearchUrl);
     }
-    console.log(search.value)
     titleSearch.innerHTML = `Результат поиска для: ${search.value}`;
-    search.value = "";
-    
+    search.value = "";   
 });
+
+
+
+//====================popup==================
+
+
+//модальное окно по клику на постер
+window.addEventListener("click", function (event) {
+  if (event.target.closest('.card')) {
+    const filmId = event.target.getAttribute('data-id');
+    const filmItem = `${API_FILM_INFO}${filmId}`;
+    
+    getMovie(filmItem);
+     
+  };
+
+});
+
+//рендер модального окна
+function showModal(item) {
+  console.log(item);
+  const parentFilm = document.querySelector('.main__films');
+  let modalFilm = `
+    <div class="modalWindow">
+      <div class="modal-content">
+        <span class="close_btn">&times;</span>
+          <div class="popup_block popup_img">
+            <img src="${item.posterUrlPreview}" alt="${item.nameRu}" class="popup_poster">
+          </div>
+          <div class="popup_text popup_block">
+            <h3 class="text_title">${item.nameRu}</h3>
+            <h4 class="text_title_original">${item.nameOriginal}
+              </h4>
+            <p class="text_description"><span class="text_text">Описание: </span>${item.description}</p>
+            <p class="text_genre"><span class="text_text">Жанр: </span>${item.genres.map((genre, index) => {
+              if (index < 3) {
+                return `${genre.genre}`;
+              }
+            })}</p>
+            <p class="text_year"><span class="text_text">Год производства: </span>${item.year}</p>
+          </div>               
+      </div>    
+    </div>
+    `;
+
+    parentFilm.innerHTML += modalFilm;
+
+  //закрытие модального окна
+  const closeBtn = document.querySelector('.close_btn');
+  const modal = document.querySelector('.modalWindow');
+    closeBtn.addEventListener('click', () => {
+      parentFilm.innerHTML -= modalFilm;
+    });
+  
+    modal.addEventListener("click", (e) => {
+      // закрытие по щелчку на пустом поле
+      if (e.target === modal) {
+        parentFilm.innerHTML -= modalFilm;
+      }
+    });
+  
+    document.addEventListener("keydown", (e) => {
+      // закрытие клавишей escape
+      if (e.code === "Escape") {
+        parentFilm.innerHTML -= modalFilm;
+      }
+    });
+};
+
+
+  
+//===========================
